c7eaa6be8539afae4b8928cd56593ff38802036b
Payton Burr <greatpayton@gmail.com>
2019-08-06 23:26:25 -0600

    added user prompts

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -35,3 +35,79 @@ let start = function() {
     promptUser(result);
   });
 };
+
+let promptUser = function(result) {
+  inquirer
+    .prompt([
+      {
+        name: 'action',
+        type: 'list',
+        message: '------------------------------------------\nWhat would you like to do?',
+        choices: ['Buy a product', 'Exit']
+      }
+    ])
+    .then(function(answer) {
+      switch (answer.action) {
+        case 'Buy a product':
+          console.log('BUYING A PRODUCT');
+          promptInput(result);
+          break;
+        case 'Exit':
+          console.log('Exiting...');
+          process.exit();
+          break;
+      }
+    });
+};
+
+let promptInput = function(result) {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'item',
+        message: 'What item would you like to buy? (Type "Exit" to exit)'
+      }
+    ])
+    .then(function(answer) {
+      for (let i = 0; i < result.length; i++) {
+        if (result[i].product_name === answer.item) {
+          correct = true;
+          let product = answer.item;
+          let id = i;
+          inquirer
+            .prompt({
+              type: 'input',
+              name: 'quantity',
+              message: 'How many would you like to buy?',
+              validate: function(value) {
+                if (isNaN(value) === false) {
+                  return true;
+                }
+                return false;
+              }
+            })
+            .then(function(answer) {
+              if (result[id].stock_quantity - answer.quantity > 0) {
+                connection.query(
+                  "UPDATE products SET stock_quantity='" +
+                    (result[id].stock_quantity - answer.quantity) +
+                    "'WHERE product_name='" +
+                    product +
+                    "'",
+                  function(err, result2) {
+                    console.log('Purchase Successful!');
+                    start();
+                  }
+                );
+              } else {
+                console.log('Input not valid');
+                promptInput(result);
+              }
+            });
+        } else if ('Exit' === answer.item) {
+          process.exit();
+        }
+      }
+    });
+};
